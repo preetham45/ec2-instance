7358fce6531edfd5df0663198668ad9d61bf18d2
preetham45 <preethamreddy45@gmail.com>
2017-03-24 19:30:55 -0400

    Update and rename LICENSE.md to ec2-launch.sh

---
+++ ec2-launch.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+ami=ami-f4cc1de2
+if [ "$ami" == "ami-f4cc1de2" ]
+then
+aws ec2 run-instances --image-id $ami --count 1 --instance-type t2.micro --key-name aws-new --region us-west-2
+else
+echo "The only supported ami is ami-f4cc1de2 and region is us-west-2"
+fi
