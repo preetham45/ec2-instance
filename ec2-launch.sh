c8a3226f50d68a8e15237a6fe514f20c050b0030
preetham45 <preethamreddy45@gmail.com>
2017-03-24 20:08:50 -0400

    Update ec2-launch.sh

--- ec2-launch.sh
+++ ec2-launch.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-ami=ami-f4cc1de2
+ami=ami-0b33d91d
 
-if [ "$ami" == "ami-f4cc1de2" ]
+if [ "$ami" == "ami-0b33d91d" ]
 then
 aws ec2 run-instances --image-id $ami --count 1 --instance-type t2.micro --key-name aws-new --region us-west-2
 else
-echo "The only supported ami is ami-f4cc1de2 and region is us-west-2"
+echo "The only supported ami is ami-0b33d91d and region is us-west-2"
 exit 1
 fi
